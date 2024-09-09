aa1e4943c0428467ca6b71e5f7357aa9ba33f3ac
cland29 <carl.lee.landskron@gmail.com>
2024-09-09 14:25:19 -0400

    Add HardwareIDMap.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/constants/HardwareIDMap.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode.constants;
+
+public class HardwareIDMap {
+
+    public static final String LEFT_FRONT_MOTOR_ID = "FL";
+    public static final String LEFT_BACK_MOTOR_ID = "BL";
+    public static final String RIGHT_FRONT_MOTOR_ID = "FR";
+    public static final String RIGHT_BACK_MOTOR_ID = "BR";
+
+    public static final String LEFT_LIFT_MOTOR_ID = "LeftLiftMotor";
+    public static final String RIGHT_LIFT_MOTOR_ID = "RightLiftMotor";
+    public static final String LEFT_BUCKET_SERVO_ID = "LeftBucketServo";
+    public static final String RIGHT_BUCKET_SERVO_ID = "RightBucketServo";
+
+    public static final String LEFT_ARM_MOTOR_ID = "LeftArmMotor";
+    public static final String RIGHT_ARM_MOTOR_ID = "RightArmMotor";
+    public static final String LEFT_INTAKE_SERV0_ID = "LeftIntakeServo";
+    public static final String RIGHT_INTAKE_SERVO_ID = "RightIntakeServo";
+
+}
