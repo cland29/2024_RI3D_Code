bef5d3bb4ac305afd2e5cee9916e1de6e9c13eef
cland29 <carl.lee.landskron@gmail.com>
2024-09-09 15:33:08 -0400

    Add SubsystemConstants.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/constants/SubsystemConstants.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode.constants;
+
+public class SubsystemConstants {
+    public static class MecanumDrivetrainConstants {
+        public static final double DRIVE_SPEED_MODIFER = 0.75;
+    }
+
+    public static class ArmConstants {
+        public static final double[] PIDF_VALUES = {10, 2, 0, 0};
+        public static final double MAX_VELOCITY = 2700;
+    }
+
+    public static class ElevatorConstants {
+        public static final double[] PIDF_VALUES = {10, 2, 0, 0};
+    }
+}
